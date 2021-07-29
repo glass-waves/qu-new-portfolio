0442b83961b91c2b524be9799669ff31c975ede6
Dylan White <dylan.whitej@gmail.com>
2021-07-29 13:51:12 -0700

    enter page

---
+++ pages/Enter.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "../styles/Enter.module.css";
+import Link from "next/link";
+
+function Enter(props) {
+  return (
+    <section className={styles.container}>
+      <div className={styles.imageContainer}>
+        <img
+          className={styles.image}
+          src="https://res.cloudinary.com/glasswavs/image/upload/v1627590500/General%20Qu/enter/ENTERpage1_1_urjjl8.png"
+        />
+        <Link href="/">
+            <img
+              className={styles.imageLink}
+              src="https://res.cloudinary.com/glasswavs/image/upload/v1627590500/General%20Qu/enter/ENTERpage1_2enter_x5tjbb.png"
+            />
+        </Link>
+        <img
+          className={styles.image}
+          src="https://res.cloudinary.com/glasswavs/image/upload/v1627590500/General%20Qu/enter/ENTERpage1_3_wykn8x.png"
+        />
+      </div>
+    </section>
+  );
+}
+
+Enter.propTypes = {};
+
+export default Enter;
