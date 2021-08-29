7359c0b7713384311ed412527d1d3cd416d0e41a
Dylan White <dylan.whitej@gmail.com>
2021-08-29 14:12:06 -0700

    switch images

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,15 +16,15 @@ function Enter(props) {
             height={501}
           />
         </div>
-        <div className={styles.imageLink}>
-              <Image
-                className={styles.hover}
-                alt="center enter image"
-                src="/../public/ENTERpage1_2enter.png"
-                width={594}
-                height={595}
-              />
+        <div className={styles.image}>
+          <Image
+            alt="left enter image"
+            src="https://res.cloudinary.com/glasswavs/image/upload/c_scale,dpr_2.0,q_auto:best,w_500/v1627590500/General%20Qu/enter/ENTERpage1_1_urjjl8.png"
+            width={500}
+            height={501}
+          />
         </div>
+
         <div className={styles.image}>
           <Image
             alt="right enter image"
