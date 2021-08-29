bfb94e2d5f70a8aea87fa7270c1c1ac510700f0f
Dylan White <dylan.whitej@gmail.com>
2021-08-29 14:14:56 -0700

    adds link back in

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,12 +17,14 @@ function Enter(props) {
           />
         </div>
         <div className={styles.image}>
-          <Image
-            alt="left enter image"
-            src="https://res.cloudinary.com/glasswavs/image/upload/c_scale,dpr_2.0,q_auto:best,w_500/v1627590500/General%20Qu/enter/ENTERpage1_1_urjjl8.png"
-            width={500}
-            height={501}
-          />
+          <Link href="/home">
+            <Image
+              alt="left enter image"
+              src="https://res.cloudinary.com/glasswavs/image/upload/c_scale,dpr_2.0,q_auto:best,w_500/v1627590500/General%20Qu/enter/ENTERpage1_1_urjjl8.png"
+              width={500}
+              height={501}
+            />
+          </Link>
         </div>
 
         <div className={styles.image}>
