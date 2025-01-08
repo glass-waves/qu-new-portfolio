2aad602f68a731bc0f0d28d7b59721709164b8e7
Derrick Bozich <dbozich1@gmail.com>
2025-01-08 15:53:40 -0700

    a few seo tweaks, minor styling tweak to about page

--- components/seo.jsx
+++ components/seo.jsx
@@ -5,6 +5,15 @@ import PropTypes from "prop-types";
 const SEO = ({ title, description, keywords }) => {
   const hasDescription = description.length > 0;
   const hasKeywords = keywords.length > 0;
+  const defaultDescription =
+    "Creative direction, set and props styling, wardrobe styling, color consulting. Based in Portland, OR.";
+
+  if (!hasDescription) {
+    description = defaultDescription;
+  }
+  const canonicalUrl =
+    typeof window !== "undefined" ? window.location.href : "";
+
   return (
     <Head>
       {/* Title */}
@@ -19,13 +28,9 @@ const SEO = ({ title, description, keywords }) => {
       />
 
       {/* Description */}
-      {hasDescription && <meta name="description" content={description} />}
-      {hasDescription && (
-        <meta property="og:description" content={description} />
-      )}
-      {hasDescription && (
-        <meta name="twitter:description" content={description} />
-      )}
+      <meta name="description" content={description} />
+      <meta property="og:description" content={description} />
+      <meta name="twitter:description" content={description} />
 
       {/* Keywords */}
       {hasKeywords && <meta name="keywords" content={keywords} />}
@@ -43,6 +48,11 @@ const SEO = ({ title, description, keywords }) => {
         name="twitter:image"
         content="https://res.cloudinary.com/glasswavs/image/upload/v1736195098/General%20Qu/about/NEWBIO_PHOTO_ulnfk4.jpg"
       />
+      <meta property="twitter:card" content="summary_large_image"></meta>
+
+      <meta property="og:site_name" content="General Qu"></meta>
+      <meta property="og:url" content={canonicalUrl}></meta>
+      <link rel="canonical" href={canonicalUrl} />
     </Head>
   );
 };
